6fa2a010fbb1595cac0f1d38bb7411a693664ab7
wangkaiwd <1162486176@qq.com>
2025-07-19 17:12:28 +0800

    feat: update project to use React Router and restructure routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import path from 'node:path'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { reactRouter } from "@react-router/dev/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/lowcode-concept/',
+  plugins: [reactRouter(), tailwindcss()],
+  base: process.env.NODE_ENV === 'development' ? '/' : '/lowcode-concept/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
