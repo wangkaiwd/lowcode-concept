a67504422034be0424e61cbbc1aebdc45da4b0c8
wangkaiwd <1162486176@qq.com>
2025-07-20 15:39:11 +0800

    feat: implement dragging overlay

--- src/core/layoutEngine.ts
+++ src/core/layoutEngine.ts
@@ -2,6 +2,7 @@ class LayoutEngine {
   isDragging = false
   currentBlockEl: HTMLElement | null = null
   dragCoordinate: any = null
+  overlay: HTMLElement | null = null
 
   constructor () {
 
@@ -26,23 +27,26 @@ class LayoutEngine {
       startBlockY: y,
     }
     this.listenToBody()
+    this.createOverlay()
   }
 
   onMouseMove = (ev: any) => {
-    if (!this.isDragging) return
-    const { startMouseX, startMouseY, startBlockX, startBlockY } = this.dragCoordinate
+    if (!this.isDragging || !this.overlay) return
+    const { startMouseX, startMouseY } = this.dragCoordinate
     const deltaX = ev.clientX - startMouseX
     const deltaY = ev.clientY - startMouseY
-    this.currentBlockEl!.style.position = 'absolute'
-    this.currentBlockEl!.style.left = startBlockX + deltaX + 'px'
-    this.currentBlockEl!.style.top = startBlockY + deltaY + 'px'
+    this.overlay.style.left = startMouseX + deltaX + 'px'
+    this.overlay.style.top = startMouseY + deltaY + 'px'
   }
   onMouseUp = () => {
     if (!this.isDragging) return
     this.isDragging = false
     this.dragCoordinate = null
-    this.currentBlockEl!.style.position = 'static'
     this.currentBlockEl = null
+    if (this.overlay) {
+      document.body.removeChild(this.overlay)
+      this.overlay = null
+    }
     document.removeEventListener('mousemove', this.onMouseMove)
     document.removeEventListener('mouseup', this.onMouseUp)
   }
@@ -50,6 +54,16 @@ class LayoutEngine {
   listenToBody = () => {
     document.addEventListener('mousemove', this.onMouseMove)
     document.addEventListener('mouseup', this.onMouseUp)
+  }
+
+  createOverlay = () => {
+    const { startMouseX, startMouseY } = this.dragCoordinate
+    this.overlay = document.createElement('div')
+    this.overlay.style.position = 'absolute'
+    this.overlay.style.left = startMouseX + 'px'
+    this.overlay.style.top = startMouseY + 'px'
+    this.overlay.innerHTML = this.currentBlockEl!.innerHTML
+    document.body.appendChild(this.overlay)
   }
 
   destroy = () => {}
